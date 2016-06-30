a9e09399580781cf2e66b5357606c5b98af3d3cb
Marcos Filho <m.filhow@gmail.com>
2016-06-30 19:46:23 -0300

    organizando componentes em pastas especificas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import ReactDOM from'react-dom'
 import React from 'react'
-import Chat from'./Chat.js'
+import Chat from'./components/Chat.js'
 
 ReactDOM.render(<Chat />, document.getElementById('app'));
